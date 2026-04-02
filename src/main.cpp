b47ac26e23bd0eb5cfa303372ea1711984d43b35
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-04-02 09:58:39 +0200

    Add configuration's structure and restart parsing logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,2 @@
 #include <iostream>
 #include <exception>
-#include "Server.hpp"
-
-int main(int argc, char *argv[]) {
-    if (argc > 2) {
-        std::cerr << "Usage: ./webserv [file.conf]" << std::endl;
-        return 1;
-    }
-    std::string fileName = argc == 1 ? "default.conf" : argv[1];
-    try {
-        Server server(fileName);
-    } catch(const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-    return 0;
-}
--- src/parseConf.cpp
+++ src/parseConf.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <stdexcept>
 #include <string>
-#include "Server.hpp"
 
 #define EXT_LEN 5
 
@@ -11,18 +10,5 @@ void checkExtension(std::string fileName) {
     std::string ext = ".conf";
     if (length < EXT_LEN || fileName.substr(length - EXT_LEN, EXT_LEN) != ext) {
         throw std::runtime_error("Error: Invalid extension");
-    }
-}
-
-void checkForServerBlock(bool *isServerPresent, std::string line) {
-    std::string serverStart = "server {";
-    std::string serverEnd = "}";
-    if (line.substr(0, serverStart.size()) == serverStart) {
-        std::cout << "start is present" << std::endl;
-        isServerPresent[0] = true;
-    }
-    if (line.substr(0, serverEnd.size()) == serverEnd) {
-        std::cout << "end is present" << std::endl;
-        isServerPresent[1] = true;
     }
 }
