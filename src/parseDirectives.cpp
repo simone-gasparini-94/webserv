44a04bd372a5735ced549e2c328ebe58cb6f0fbf
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-04-07 10:54:58 +0200

    Check .conf validity

--- src/parseDirectives.cpp
+++ src/parseDirectives.cpp
@@ -15,25 +15,59 @@ bool isClosing(std::string line) {
     return true;
 }
 
+void checkOnlySpaces(std::string line, size_t start, size_t end) {
+    if (start > end) throw std::runtime_error("Error: parsing failure");
+    for (size_t i = start; i < end; i++) {
+        if (!isspace(line[i])) throw std::runtime_error("Error: invalid token");
+    }
+}
+
 bool isOpening(std::string line) {
     size_t pos = line.find("{");
     if (pos == line.npos) return false;
-    for (size_t i = pos + 1; i < line.length(); i++) {
-        if (!isspace(line[i])) throw std::runtime_error("Error: invalid token");
-    }
+    checkOnlySpaces(line, pos + 1, line.length());
     return true;
+}
+
+
+void checkEndPoint(std::string line, size_t start, size_t end) {
+    if (start > end) throw std::runtime_error("Error: parsing failure");
+    bool found = false;
+    for (size_t i = start; i < end; i++) {
+        if (!isspace(line[i])) {
+            if (line[i] != '/') throw std::runtime_error("Error: invalid location's endpoint");
+            found = true;
+            while (i < end && !isspace(line[i])) i++;
+            while (i < end) {
+                if (!isspace(line[i])) {
+                    throw std::runtime_error("Error: invalid token");
+                }
+                i++;
+            }
+        }
+    }
+    if (!found) throw std::runtime_error("Error: location's endpoint not specified");
+}
+
+void checkBetweenDirectiveAndBrace(std::string line, size_t start, size_t end, std::string type) {
+    if (start > end) throw std::runtime_error("Error: parsing failure");
+    if (type == "server") {
+        checkOnlySpaces(line, start, end);
+    } else if (type == "location") {
+        checkEndPoint(line, start, end);
+    }
 }
 
 bool isBlock(std::string line, std::string keyword) {
     size_t pos = line.find(keyword);
     if (pos == line.npos) return false;
-    for (size_t i = 0; i < pos; i++) {
-        if (!isspace(line[i])) throw std::runtime_error("Error: invalid token");
-    }
+    checkOnlySpaces(line, 0, pos);
     size_t start = pos + keyword.length();
     size_t end = line.find("{");
-    for (size_t i = start; i < end; i++) {
-        if (!isspace(line[i])) throw std::runtime_error("Error: invalid token");
+    if (keyword == "server") {
+        checkBetweenDirectiveAndBrace(line, start, end, "server");
+    } else if (keyword == "location") {
+        checkBetweenDirectiveAndBrace(line, start, end, "location");
     }
     return true;
 }
@@ -50,6 +84,10 @@ void parseDirective(std::string &line, Block &block) {
     (void)block;
 }
 
+void storeEndPoint(LocationBlock &location, std::string line) {
+    size_t start = line.find("location");
+}
+
 void parseDirectives(Block &block, std::ifstream &file, int level) {
     if (level == 3) return;
     if (level != block.type) throw std::runtime_error("Error: invalid token");
@@ -64,9 +102,11 @@ void parseDirectives(Block &block, std::ifstream &file, int level) {
                 block.add(server);
             } else if (isBlock(line, "location")) {
                 LocationBlock location;
-                //add endpoint to location
+                storeEndPoint(location, line);
                 parseDirectives(location, file, level + 1);
                 block.add(location);
+            } else {
+                throw std::runtime_error("Error: invalid token");
             }
         } else if (isNotEmpty(line)) {
             parseDirective(line, block);
--- tests/src/testParseDirectives.cpp
+++ tests/src/testParseDirectives.cpp
@@ -22,6 +22,7 @@ void testParseDirectives(int N, std::string argument, std::string string) {
         parseDirectives(main, file, 0);
         std::cout << N << ": " << pass << std::endl;
     } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
         std::cout << N << ": " << except << std::endl;
     }
 }
